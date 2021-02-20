a7f178a06700c7566c3b206f7212cb031c9fe4a8
Brian Hart <brian.hart@xylogix.net>
2021-02-20 12:05:35 -0500

    Create ScraperServiceFactory class

---
+++ xyLOGIX.Data.Scrapers.Services.Factories/ScraperServiceFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace xyLOGIX.Data.Scrapers.Services.Factories
+{
+    /// <summary>
+    /// Creates instances of objects that implement the
+    /// <see
+    ///     cref="T:xyLOGIX.Data.Scrapers.Interfaces.IScraperService" />
+    /// interface.
+    /// </summary>
+    public class ScraperServiceFactory : IScraperServiceFactory
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Data.Scrapers.Interfaces.IScraperService" />
+        /// interface and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to a new instance of an object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Data.Scrapers.Interfaces.IScraperService" />
+        /// interface.
+        /// </returns>
+        public IScraperService Make()
+            => new ScraperService();
+    }
+}

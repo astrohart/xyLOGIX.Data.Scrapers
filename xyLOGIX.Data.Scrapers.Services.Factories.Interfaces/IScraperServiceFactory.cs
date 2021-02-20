1ccd2699da48bbfe5cd03ee294ccb01e927eed9d
Brian Hart <brian.hart@xylogix.net>
2021-02-20 12:04:02 -0500

    Create IScraperServiceFactory interface

---
+++ xyLOGIX.Data.Scrapers.Services.Factories.Interfaces/IScraperServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿using xyLOGIX.Data.Scrapers.Interfaces;
+
+namespace xyLOGIX.Data.Scrapers.Services.Factories.Interfaces
+{
+    /// <summary>
+    /// Defines the public-exposed methods and properties of a scraper-service
+    /// factory object.
+    /// </summary>
+    public interface IScraperServiceFactory
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Data.Scrapers.Interfaces.IScraperService" />
+        /// interface and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to a new instance of an object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Data.Scrapers.Interfaces.IScraperService" />
+        /// interface.
+        /// </returns>
+        IScraperService Make();
+    }
+}

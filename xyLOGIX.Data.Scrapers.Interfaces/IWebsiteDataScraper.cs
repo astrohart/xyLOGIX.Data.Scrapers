faae6894f827bdd9c486bc48fcd86b0b8286cdb2
Brian Hart <brian.hart@xylogix.net>
2023-08-16 10:23:56 -0600

    Latest updates

--- xyLOGIX.Data.Scrapers.Interfaces/IWebsiteDataScraper.cs
+++ xyLOGIX.Data.Scrapers.Interfaces/IWebsiteDataScraper.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using xyLOGIX.Data.Scrapers.Constants;
 using xyLOGIX.Data.Scrapers.Events;
--- xyLOGIX.Data.Scrapers/WebsiteDataScraperBase.cs
+++ xyLOGIX.Data.Scrapers/WebsiteDataScraperBase.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using xyLOGIX.Core.Debug;
 using xyLOGIX.Data.Paginators.Interfaces;
